eec4386c286df694ca7e7d59bba67851cc299ea2
sorowar.amega@gmail.com <mhoss147@my.centennialcollege.ca>
2020-08-30 11:52:27 -0400

    Student-Database-App required classed, variables created

--- Student.java
+++ Student.java
@@ -1,5 +1,30 @@
 package studentDatabaseApp;
 
 public class Student {
+	
+	private String firstName;
+	private String lastName;
+	private String gradeYear;
+	private int studentID;
+	private String courses;
+	private int costOfCourse = 600;
+	private int tuitionBalance;
+	
+	
+	
+	
+	// Constructor: Prompt user to enter student's name and year
+	
+	
+	// Generate an ID
+	
+	
+	// Enroll in courses
+	
+	
+	// View balances and pay tuition
+	
+	
+	// Show status of the students with...
 
 }
