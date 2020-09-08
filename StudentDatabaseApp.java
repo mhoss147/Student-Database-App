86e3b387aca1cdf6c7f798904877eaa2d4354f81
sorowar.amega@gmail.com <mhoss147@my.centennialcollege.ca>
2020-09-07 22:23:09 -0400

    Three students created

--- StudentDatabaseApp.java
+++ StudentDatabaseApp.java
@@ -4,12 +4,19 @@ public class StudentDatabaseApp {
 
 	public static void main(String[] args) {
 		Student student1 = new Student();
-		
 		student1.enroll();
-		
 		student1.payTuition();
 		System.out.println(student1.showInfo());		
 
+		Student student2 = new Student();
+		student2.enroll();
+		student2.payTuition();
+		System.out.println(student2.showInfo());
+		
+		Student student3 = new Student();
+		student3.enroll();
+		student3.payTuition();
+		System.out.println(student3.showInfo());
 		
 		// Ask how many New users we want to add
 		
